fe2ee1055bd22f55db83a0f9eb07617a5b4ab2c3
Abhikriti Moti <abhikriti.12007073@lpu.in>
2023-10-16 08:53:59 +0530

    Add files via upload

---
+++ Contact.kt
@@ -0,0 +1,14 @@
+package com.example.l_roomdatabase
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "contact")
+
+data class Contact(
+    @PrimaryKey(autoGenerate = true)
+    val id:Long,
+    val name: String,
+    val phone: Long
+)
